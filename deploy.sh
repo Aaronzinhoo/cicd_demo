e9c1d118081fe143fd093960f64e065393ea6867
Aaronzinhoo <aaronzinho@ucla.edu>
2020-07-29 02:28:22 -0700

    add deployment on aws

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+# install AWS CLI
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+
+# get public IP of CircleCI runner
+RUNNER_IP=$(curl ipinfo.io/ip)
+
+# AWS Configuration
+EC2_USERNAME=ubuntu
+EC2_PUBLIC_IP=34.209.142.0
+AWS_REGION=us-west-2
+SECURITY_GROUP_ID=sg-009d6969584c6f353
+
+# add incoming traffic rule (ingress rule)
+~/bin/aws ec2 authorize-security-group-ingress --region $AWS_REGION --group-id $SECURITY_GROUP_ID --protocol tcp --port 22 \
+          --cidr $RUNNER_IP/24
+
+# give ingrees rule time to propagate
+sleep 7
+
+# steps to deploy
+# docker should already be installed, just need to copy repo code
+
+scp -r $CIRCLE_PROJECT_REPONAME $EC2_USERNAME@$EC2_PUBLIC_IP:~/
+ssh -o StrictHostKeyChecking=no $EC2_USERNAME@$EC2_PUBLIC_IP \
+    cd $CIRCLE_PROJECT_REPONAME \
+    npm run build:prod \
+    npm run start:prod
+
+ECHO "DEPLOYED "
+
+# remove ingress rule
+~/bin/aws ec2 revoke-security-group-ingress --region $AWS_REGION --group-id $SECURITY_GROUP_ID \
+        --protocol tcp --port 22 --cidr $RUNNER_IP
