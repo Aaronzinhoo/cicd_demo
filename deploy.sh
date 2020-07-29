b740a000d5705acd6ed1d65b94c8314e08302c42
Aaronzinhoo <aaronzinho@ucla.edu>
2020-07-29 02:41:33 -0700

    update deployment config

--- deploy.sh
+++ deploy.sh
@@ -26,9 +26,8 @@ scp -r $CIRCLE_PROJECT_REPONAME $EC2_USERNAME@$EC2_PUBLIC_IP:~/
 ssh -o StrictHostKeyChecking=no $EC2_USERNAME@$EC2_PUBLIC_IP \
     cd $CIRCLE_PROJECT_REPONAME \
     npm run build:prod \
-    npm run start:prod
-
-ECHO "DEPLOYED "
+    npm run start:prod \
+    echo "DEPLOYED ${CIRCLE_PROJECT_REPONAME} SUCCESFULLY"
 
 # remove ingress rule
 ~/bin/aws ec2 revoke-security-group-ingress --region $AWS_REGION --group-id $SECURITY_GROUP_ID \
